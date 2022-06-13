269ab3224b030cb98b53ca1085a73bee96f2812f
christianhaitian <mckman@comcast.net>
2022-06-13 18:05:08 -0400

    Update pcsx_rearmed.sh

--- scripts/pcsx_rearmed.sh
+++ scripts/pcsx_rearmed.sh
@@ -12,33 +12,26 @@
 cur_wd="$PWD"
 bitness="$(getconf LONG_BIT)"
 
-	# Libretro Pcsx_rearmed build
-	if [[ "$var" == "pcsx_rearmed" || "$var" == "all" ]]; then
-	 pcsx_rearmed_rumblepatch="no"
+	# Libretro Retroarch build
+	if [[ "$var" == "retroarch" ]]; then
 	 cd $cur_wd
-	  if [ ! -d "pcsx_rearmed/" ]; then
-		git clone https://github.com/libretro/pcsx_rearmed.git
+	  if [ ! -d "retroarch/" ]; then
+		git clone https://github.com/libretro/retroarch.git
 		if [[ $? != "0" ]]; then
 		  echo " "
-		  echo "There was an error while cloning the pcsx_rearmed libretro git.  Is Internet active or did the git location change?  Stopping here."
+		  echo "There was an error while cloning the retroarch libretro git.  Is Internet active or did the git location change?  Stopping here."
 		  exit 1
 		fi
-		cp patches/pcsx_rearmed-patch* pcsx_rearmed/.
+		cp patches/retroarch-patch* retroarch/.
 	  fi
 
-	 cd pcsx_rearmed/
-
-	 pcsx_rearmed_patches=$(find *.patch)
-
-	 if [[ ! -z "$pcsx_rearmed_patches" ]]; then
-	  for patching in pcsx_rearmed-patch*
+	 cd retroarch/
+	 
+	 retroarch_patches=$(find *.patch)
+	 
+	 if [[ ! -z "$retroarch_patches" ]]; then
+	  for patching in retroarch-patch*
 	  do
-		 if [[ $patching == *"rumble"* ]]; then
-		   echo " "
-		   echo "Skipping the $patching for now and making a note to apply that later"
-		   sleep 3
-		   pcsx_rearmed_rumblepatch="yes"
-		 else  
 		   patch -Np1 < "$patching"
 		   if [[ $? != "0" ]]; then
 			echo " "
@@ -46,100 +39,99 @@ bitness="$(getconf LONG_BIT)"
 			exit 1
 		   fi
 		   rm "$patching" 
-		 fi
 	  done
 	 fi
-	  make -f Makefile.libretro clean
-
-	  sed -i '/a53/s//a55/g' Makefile.libretro
- 	  sed -i '/a72/s//a55/g' Makefile.libretro
-
 	  if [[ "$bitness" == "64" ]]; then
-	    make -f Makefile.libretro ARCH=arm64 BUILTIN_GPU=unai DYNAREC=ari64 platform=rk3326_64 -j$(nproc)
-	  else
-	    make -f Makefile.libretro HAVE_NEON=1 ARCH=arm BUILTIN_GPU=neon DYNAREC=ari64 platform=rk3326 -j$(nproc)
-	  fi
+	    CFLAGS="-O2 -march=armv8-a+crc -mtune=cortex-a55 -ftree-vectorize -funsafe-math-optimizations" ./configure --disable-caca \
+	    --disable-mali_fbdev \
+	    --disable-opengl \
+	    --disable-opengl1 \
+	    --disable-qt \
+	    --disable-sdl \
+	    --disable-vg \
+	    --disable-vulkan \
+	    --disable-vulkan_display \
+	    --disable-wayland \
+	    --disable-x11 \
+	    --enable-alsa \
+	    --enable-egl \
+	    --enable-freetype \
+	    --enable-kms \
+	    --enable-networking \
+	    --enable-odroidgo2 \
+	    --enable-opengles \
+	    --enable-opengles3 \
+	    --enable-opengles3_1 \
+	    --enable-opengles3_2 \
+	    --enable-ozone \
+	    --enable-udev \
+	    --enable-wifi
+      else
+	    CFLAGS="-O2 -march=armv8-a+crc -mtune=cortex-a55 -mfpu=neon-fp-armv8 -mfloat-abi=hard -ftree-vectorize -funsafe-math-optimizations" ./configure --disable-caca \
+	    --disable-mali_fbdev \
+	    --disable-opengl \
+	    --disable-opengl1 \
+	    --disable-qt \
+	    --disable-sdl \
+	    --disable-vg \
+	    --disable-vulkan \
+	    --disable-vulkan_display \
+	    --disable-wayland \
+	    --disable-x11 \
+	    --enable-alsa \
+	    --enable-egl \
+	    --enable-freetype \
+	    --enable-kms \
+        --enable-neon \
+	    --enable-networking \
+	    --enable-odroidgo2 \
+	    --enable-opengles \
+	    --enable-opengles3 \
+	    --enable-opengles3_1 \
+	    --enable-opengles3_2 \
+	    --enable-ozone \
+	    --enable-udev \
+	    --enable-wifi
+      fi
+
+	  make -j$(nproc)
 
 	  if [[ $? != "0" ]]; then
 		echo " "
-		echo "There was an error while building the newest lr-pcsx_rearmed core.  Stopping here."
+		echo "There was an error while building the newest retroarch.  Stopping here."
 		exit 1
 	  fi
 
-	  strip pcsx_rearmed_libretro.so
+	  strip retroarch
 
-	  if [ ! -d "../cores$bitness/" ]; then
-		mkdir -v ../cores$bitness
+	  if [ ! -d "../retroarch$bitness/" ]; then
+		mkdir -v ../retroarch$bitness
 	  fi
 
-	  mv pcsx_rearmed_libretro.so ../cores$bitness/.
+	  cp retroarch ../retroarch$bitness/.
 
-          if [[ "$bitness" == "32" ]]; then
-            make -f Makefile.libretro clean
-            make -f Makefile.libretro HAVE_NEON=1 ARCH=arm BUILTIN_GPU=peops THREAD_RENDERING=1 DYNAREC=ari64 platform=rk3326
-            if [[ $? != "0" ]]; then
-                  echo " "
-                  echo "There was an error while building the newest lr-pcsx_rearmed_peops core.  Stopping here."
-                  exit 1
-            fi
-            mv pcsx_rearmed_libretro.so ../cores$bitness/pcsx_rearmed_peops_libretro.so
-            gitcommit=$(git log | grep -m 1 commit | cut -c -14 | cut -c 8-)
-            echo $gitcommit > ../cores$bitness/pcsx_rearmed_peops_libretro.so.commit
-          fi
-
-	  if [[ $pcsx_rearmed_rumblepatch == "yes" ]]; then
-		for patching in pcsx_rearmed-patch*
-		do
-		  patch -Np1 < "$patching"
-		  if [[ $? != "0" ]]; then
-			echo " "
-			echo "There was an error while patching in the rumble feature from $patching.  Stopping here."
-			exit 1
-		  fi
-		  rm "$patching"
-
-		  make -f Makefile.libretro clean
-		  if [[ "$bitness" == "64" ]]; then
-		    make -f Makefile.libretro ARCH=arm64 BUILTIN_GPU=unai DYNAREC=ari64 platform=rk3326_64 -j$(nproc)
-		  else
-		    make -f Makefile.libretro HAVE_NEON=1 ARCH=arm BUILTIN_GPU=neon DYNAREC=ari64 platform=rk3326 -j$(nproc)
-		  fi
-
-		  if [[ $? != "0" ]]; then
-			echo " "
-			echo "There was an error while building the newest lr-pcsx_rearmed core with the patched in rumble feature.  Stopping here."
-			exit 1
-		  fi
-
-		  strip pcsx_rearmed_libretro.so
-		  mv pcsx_rearmed_libretro.so pcsx_rearmed_rumble_libretro.so
-		  cp pcsx_rearmed_rumble_libretro.so ../cores$bitness/.
-		  echo " "
-		  echo "pcsx_rearmed_libretro.so and pcsx_rearmed_rumble_libretro.so have been created and have been placed in the rk3326_core_builds/cores32 subfolder"
-	      gitcommit=$(git log | grep -m 1 commit | cut -c -14 | cut -c 8-)
-	      echo $gitcommit > ../cores$bitness/pcsx_rearmed_rumble_libretro.so.commit
-		done
+	  if [[ "$bitness" == "32" ]]; then
+		mv ../retroarch$bitness/retroarch ../retroarch$bitness/retroarch32
 	  fi
-
-	  make -f Makefile.libretro clean
-          if [[ "$bitness" == "32" ]]; then
-            make -f Makefile.libretro HAVE_NEON=1 ARCH=arm BUILTIN_GPU=peops THREAD_RENDERING=1 DYNAREC=ari64 platform=rk3326
-            if [[ $? != "0" ]]; then
-                  echo " "
-                  echo "There was an error while building the newest lr-pcsx_rearmed_rumble_peops core.  Stopping here."
-                  exit 1
-            fi
-            mv pcsx_rearmed_libretro.so ../cores$bitness/pcsx_rearmed_rumble_peops_libretro.so
-            gitcommit=$(git log | grep -m 1 commit | cut -c -14 | cut -c 8-)
-            echo $gitcommit > ../cores$bitness/$(basename $PWD)_libretro.so.commit
-
-            echo " "
-            echo "pcsx_rearmed_libretro.so has been created and has been placed in the rk3326_core_builds/cores32 subfolder"
-          fi
-
-	  gitcommit=$(git log | grep -m 1 commit | cut -c -14 | cut -c 8-)
-	  echo $gitcommit > ../cores$bitness/$(basename $PWD)_rumble_peops_libretro.so.commit
 
 	  echo " "
-	  echo "pcsx_rearmed_libretro.so has been created and has been placed in the rk3326_core_builds/cores32 subfolder"
+	  if [[ "$bitness" == "32" ]]; then
+		echo "retroarch32 has been created and has been placed in the rk3326_core_builds/retroarch$bitness subfolder"
+	  else
+		echo "retroarch has been created and has been placed in the rk3326_core_builds/retroarch$bitness subfolder"
+	  fi
+
+	  cd gfx/video_filters
+	  ./configure
+	  make -j$(nproc)
+	  if [[ $? != "0" ]]; then
+		echo " "
+		echo "There was an error while building the video filters for retroarch.  Stopping here."
+		exit 1
+	  fi
+	  mkdir -p ../../../retroarch$bitness/filters/video
+	  cp *.so ../../../retroarch$bitness/filters/video
+	  cp *.filt ../../../retroarch$bitness/filters/video
+	  echo " "
+	  echo "Video filters have been built and copied to the rk3326_core_builds/retroarch$bitness/filters/video subfolder"
 	fi
