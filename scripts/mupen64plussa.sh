7775b52396d4e57f8ff25a6d204ff4866060c680
christianhaitian <christianhaitian0@gmail.com>
2022-08-14 22:11:56 +0000

    Add GLideN64 plugin build for mupen64plus standalone thanks to @rishooty

--- scripts/mupen64plussa.sh
+++ scripts/mupen64plussa.sh
@@ -3,7 +3,7 @@
 ##################################################################
 # Created by Christian Haitian for use to easily update          #
 # various standalone emulators, libretro cores, and other        #
-# various programs for the RK3326 platform for various Linux     #
+# various programs for the rk3566 platform for various Linux     #
 # based distributions.                                           #
 # See the LICENSE.md file at the top-level directory of this     #
 # repository.                                                    #
@@ -27,6 +27,8 @@ bitness="$(getconf LONG_BIT)"
    source scripts/mupen64plus-video-rice.sh
    cd $cur_wd
    source scripts/mupen64plus-video-glide64mk2.sh
+   cd $cur_wd
+   source scripts/mupen64plus-video-gliden64.sh
 
    cd $cur_wd
    gitcommit=$(git --git-dir mupen64plus-core/.git log | grep -m 1 commit | cut -c -14 | cut -c 8-)
