547f5cff1beae45293cd56e4f624c57859096154
christianhaitian <mckman@comcast.net>
2022-11-30 21:29:11 -0500

    Added workaround for connecting to wpa2/wpa3 networks

--- shell-scripts/503-only/Wifi.sh
+++ shell-scripts/503-only/Wifi.sh
@@ -28,6 +28,16 @@ printf "\033c" > /dev/tty1
 
 # hide cursor
 printf "\e[?25l" > /dev/tty1
+dialog --clear
+
+height="15"
+width="55"
+
+if test ! -z "$(cat /home/ark/.config/.DEVICE | grep RG503 | tr -d '\0')"
+then
+  height="20"
+  width="60"
+fi
 
 export TERM=linux
 export XDG_RUNTIME_DIR=/run/user/$UID/
@@ -52,7 +62,7 @@ DeleteConnect() {
 
   cur_ap=`iw dev wlan0 info | grep ssid | cut -c 7-30`
   dialog --clear --backtitle "Delete Connection: Currently connected to $cur_ap" --title "Removing $1" --clear \
-  --yesno "\nWould you like to continue to remove this connection?" 15 55 2>&1 > /dev/tty1
+  --yesno "\nWould you like to continue to remove this connection?" $height $width 2>&1 > /dev/tty1
 
   case $? in
      0) sudo rm -f /etc/NetworkManager/system-connections/$1.nmconnection ;;
@@ -73,7 +83,7 @@ Activate() {
    	--no-collapse \
    	--clear \
 	--cancel-label "Back" \
-    --menu "" 15 55 15)
+    --menu "" $height $width 15)
 
     achoices=$("${aselection[@]}" "${aoptions[@]}" 2>&1 > /dev/tty1) || MainMenu
 
@@ -92,12 +102,21 @@ Select() {
   # get password from input
   PASS=`$KEYBOARD "Enter Wi-Fi password" | tail -n 1`
 
-  dialog --infobox "\nConnecting to Wi-Fi $1 ..." 5 55 > /dev/tty1
+  dialog --infobox "\nConnecting to Wi-Fi $1 ..." 5 $width > /dev/tty1
+  clist2=`sudo nmcli -f ALL --mode tabular --terse --fields IN-USE,SSID,CHAN,SIGNAL,SECURITY dev wifi`
+  WPA3=`echo "$clist2" | grep "$1" | grep " WPA3"`
 
   # try to connect
   output=`nmcli con delete "$1"`
-  output=`nmcli device wifi connect "$1" password "$PASS"`
-
+  if [[ "$WPA3" != *" WPA3"* ]]; then
+    output=`nmcli device wifi connect "$1" password "$PASS"`
+  else
+    #workaround for wpa2/wpa3 connectivity
+    output=`nmcli device wifi connect "$1" password "$PASS"`
+    sudo sed -i '/key-mgmt\=sae/s//key-mgmt\=wpa-psk/' /etc/NetworkManager/system-connections/"$1".nmconnection
+    sudo systemctl restart NetworkManager
+    output=`nmcli con up "$1"`
+  fi
   success=`echo "$output" | grep successfully`
 
   if [ -z "$success" ]; then
@@ -107,7 +126,7 @@ Select() {
 	cur_ap=`iw dev wlan0 info | grep ssid | cut -c 7-30`
   fi
   
-  dialog --infobox "\n$output" 6 55 > /dev/tty1
+  dialog --infobox "\n$output" 6 $width > /dev/tty1
   sleep 3
   Connect
 }
@@ -123,7 +142,7 @@ Select() {
 ConnectExisting() {
   cur_ap=`iw dev wlan0 info | grep ssid | cut -c 7-30`
 
-  dialog --infobox "\nConnecting to Wi-Fi $1 ..." 5 55 > /dev/tty1
+  dialog --infobox "\nConnecting to Wi-Fi $1 ..." 5 $width > /dev/tty1
 
   nmcli con down "$cur_ap" >> /dev/null
   sleep 1
@@ -139,14 +158,14 @@ ConnectExisting() {
 	cur_ap=`iw dev wlan0 info | grep ssid | cut -c 7-30`
   fi
   
-  dialog --infobox "\n$output" 6 55 > /dev/tty1
+  dialog --infobox "\n$output" 6 $width > /dev/tty1
   sleep 3
   Activate
 }
 
 Connect() {
-  dialog --infobox "\nScanning available Wi-Fi access points ..." 5 55 > /dev/tty1
-  sleep 5
+  dialog --infobox "\nScanning available Wi-Fi access points ..." 5 $width > /dev/tty1
+  sleep 1
   clist=`sudo nmcli -f ALL --mode tabular --terse --fields IN-USE,SSID,CHAN,SIGNAL,SECURITY dev wifi`
   cur_ap=`iw dev wlan0 info | grep ssid | cut -c 7-30`
 
@@ -173,7 +192,7 @@ Connect() {
    	--no-collapse \
    	--clear \
 	--cancel-label "Back" \
-    --menu "" 15 55 15)
+    --menu "" $height $width 15)
 
     cchoices=$("${cselection[@]}" "${coptions[@]}" 2>&1 > /dev/tty1) || MainMenu
 
@@ -196,7 +215,7 @@ Delete() {
    	--no-collapse \
    	--clear \
 	--cancel-label "Back" \
-    --menu "" 15 55 15)
+    --menu "" $height $width 15)
 
     delchoices=$("${delselection[@]}" "${deloptions[@]}" 2>&1 > /dev/tty1) || MainMenu
 
@@ -217,7 +236,7 @@ NetworkInfo() {
   currentdns=`( nmcli dev list || nmcli dev show ) 2>/dev/null | grep DNS | awk '{print $2}'`
 
   dialog --clear --backtitle "Your Network Information" --title "" --clear \
-  --msgbox "\n\nSSID: $cur_ap\nIP: $currentip\nGateway: $gateway\nDNS: $currentdns" 15 55 2>&1 > /dev/tty1
+  --msgbox "\n\nSSID: $cur_ap\nIP: $currentip\nGateway: $gateway\nDNS: $currentdns" $height $width 2>&1 > /dev/tty1
 }
 
 MainMenu() {
@@ -231,7 +250,7 @@ MainMenu() {
    	--no-collapse \
    	--clear \
 	--cancel-label "Select + Start to Exit" \
-    --menu "Please make your selection" 15 55 15)
+    --menu "Please make your selection" $height $width 15)
 	
 	mainchoices=$("${mainselection[@]}" "${mainoptions[@]}" 2>&1 > /dev/tty1)
 
