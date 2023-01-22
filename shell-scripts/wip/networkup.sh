337a04a633f0fa3276e904ed4ae1d751131697e3
christianhaitian <mckman@comcast.net>
2023-01-22 00:52:34 -0500

    Update networkup.sh

--- shell-scripts/wip/networkup.sh
+++ shell-scripts/wip/networkup.sh
@@ -1,30 +1,50 @@
 #!/bin/bash
 
-## This script is designed to just reconnect to a wireless network if the existing connection stops working since networkmanager is usually not running
-## which can impact the ability for a device to reconnect on it's own.
+# This script is designed to just reconnect to a wireless network if the existing connection stops working since networkmanager is usually not running
+# which can impact the ability for a device to reconnect on it's own.
 
-address="your ssid"
-bak_ap="backup ssid"
+address="ip or dns name that should be reachable"
+pri_ap="your main ssid"
+bak_ap="you backup ssid if you have one"
+LOG="/home/ark/wlan_reconnection.log"
 
 for (( ; ; ))
 do
-  if test ! -z "$(sudo ifconfig | grep wlan | tr -d '\0')"
+  #Keep the log size in check
+  if [ -f "$LOG" ]; then
+    logsizecheck=$(stat -c %s "$LOG")
+    if [ $logsizecheck -gt "1500000" ]; then
+      sudo mv -f "$LOG" "$LOG".$(date +'%H_%M_%m%d%Y').bak
+    fi
+  fi
+  #Now we check how our wireless network connection is doing
+  if test ! -z "$(sudo ifconfig | grep wlan | tr -d '\0')" 
   then
-    sudo ping -c 1 "$address"
+    sudo ping -q -c 1 "$address" > /dev/null
     if [ $? -ne 0 ]; then
           sudo systemctl stop networkwatchdaemon
-          sudo systemctl start NetworkManager
-          cur_ap=`iw dev wlan0 info | grep ssid | cut -c 7-30`
-          nmcli con down "$cur_ap"
-          nmcli con up "$cur_ap"
-          if [ $? -ne 0 ]; then
-            nmcli con up "$bak_ap"
-            echo "Just finished connecting to $bak_ap" >> /home/ark/wlan_reconnection.log
+	  sudo systemctl start NetworkManager
+	  cur_ap=`iw dev wlan0 info | grep ssid | cut -c 7-30`
+	  nmcli con down "$cur_ap"
+	  nmcli con up "$cur_ap"
+	  if [ $? -ne 0 ]; then
+	    nmcli con up "$pri_ap"
+            if [ $? -ne 0 ]; then
+              nmcli con up "$bak_ap"
+              if [ $? -ne 0 ]; then
+                echo "Couldn't successfully connect to any ap as of $(date +'%r on %A, %B %d, %Y').  Will try again in 10 seconds" >> "$LOG"
+              else
+                echo "Just finished connecting to $bak_ap as of $(date +'%r on %A, %B %d, %Y')" >> "$LOG"
+              fi
+            else
+	      echo "Just finished connecting to $pri_ap as of $(date +'%r on %A, %B %d, %Y')" >> "$LOG"
+            fi
           else
-            echo "Just finished connecting to $cur_ap" >> /home/ark/wlan_reconnection.log
+            echo "Just finished connecting to $cur_ap as of $(date +'%r on %A, %B %d, %Y')" >> "$LOG"
           fi
           sudo systemctl stop NetworkManager
           sudo systemctl start networkwatchdaemon
+          sleep 10
     else
       echo "networkup: We're still good with wireless network connectivity" > /dev/kmsg
       sleep 60
