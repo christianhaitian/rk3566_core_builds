0400e1c384ac3259929833133c69f68e03088472
christianhaitian <mckman@comcast.net>
2023-02-05 19:57:46 -0500

    Update hdmi-test.sh

--- shell-scripts/hdmi-test.sh
+++ shell-scripts/hdmi-test.sh
@@ -3,6 +3,6 @@
 xres="$(cat /sys/class/graphics/fb0/modes | grep -o -P '(?<=:).*(?=p-)' | cut -dx -f1)"
 if [ $xres -eq "1280" ]; then
   echo 6 | sudo tee /var/run/drmMode
-elif [ $xres -eq "1920" ]; then
+else
   echo 0 | sudo tee /var/run/drmMode
 fi
