9eb9a5bdc825b4568a6181b1d3bde2b2a7c2f2d6
christianhaitian <mckman@comcast.net>
2023-09-30 02:58:42 -0400

    Update checknswitchforusbdac.sh

--- shell-scripts/checknswitchforusbdac.sh
+++ shell-scripts/checknswitchforusbdac.sh
@@ -2,6 +2,7 @@
 
 DAC="0"
 DAC_EXIST=""
+NUM_CHECK='^[1-9]+$'
 
 for (( ; ; ))
 do
@@ -12,14 +13,17 @@ if [ ! -e "/dev/snd/controlC7" ] && [[ "$DAC_EXIST" != "None" ]]; then
   DAC_EXIST="None"
 elif [ -e "/dev/snd/controlC7" ] && [[ "$DAC" != "$DAC_EXIST" ]]; then
   DAC=$(ls -l /dev/snd/controlC7 | awk 'NR>=control {print $11;}' | cut -d 'C' -f2)
-  readarray -t USB_DAC < <(amixer -q -c ${DAC} scontents | grep 'Simple mixer control ' | cut -d "'" -f2)
-  for i in "${USB_DAC[@]}"
-  do
-    amixer -q -c ${DAC} sset "${i}" 100%
-  done
-  sed -i '/hw:[0-9]/s//hw:'$DAC'/' /home/ark/.asoundrc /home/ark/.asoundrcbak
-  sudo systemctl restart oga_events &
-  DAC_EXIST="$DAC"
+  if [[ $DAC =~ $NUM_CHECK ]]; then
+    readarray -t USB_DAC < <(amixer -q -c ${DAC} scontents | grep 'Simple mixer control ' | cut -d "'" -f2)
+    for i in "${USB_DAC[@]}"
+    do
+      amixer -q -c ${DAC} sset "${i}" 100%
+    done
+    sed -i '/hw:[0-9]/s//hw:'$DAC'/' /home/ark/.asoundrc /home/ark/.asoundrcbak
+    sudo systemctl restart oga_events &
+    DAC_EXIST="$DAC"
+  fi
 fi
- sleep 1
+sleep 1
+
 done
