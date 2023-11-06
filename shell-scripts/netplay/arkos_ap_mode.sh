a7a29d1df9fb0ebcc9a8e685c50b7031e163f975
christianhaitian <mckman@comcast.net>
2023-11-06 09:48:02 -0500

    Update arkos_ap_mode.sh to accept 2nd variable for setting AP Name

--- shell-scripts/netplay/arkos_ap_mode.sh
+++ shell-scripts/netplay/arkos_ap_mode.sh
@@ -1,33 +1,15 @@
 #!/bin/bash
 
-#Copyright 2023 Christian_Haitian
-#
-#Permission is hereby granted, free of charge, to any person 
-#obtaining a copy of this software and associated documentation
-#files (the “Software”), to deal in the Software without 
-#restriction, including without limitation the rights to use,
-#copy, modify, merge, publish, distribute, sublicense, and/or
-#sell copies of the Software, and to permit persons to whom
-#the Software is furnished to do so, subject to the following conditions:
-#
-#The above copyright notice and this permission notice shall be included 
-#in all copies or substantial portions of the Software.
-#
-#THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND,
-#EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
-#OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-#IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
-#DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
-#OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR
-#THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-#
-#
 # AP mode for use with local netplay in ArkOS
-#
 
 function Enable {
   nmcli d disconnect wlan0 > /dev/null
   host_ap_masked=$(systemctl status hostapd.service | grep masked)
+  if [ ! -z $1 ]; then
+    sudo sed -i "/ssid\=/c\ssid\=ArkOS_AP_$1" /etc/hostapd/hostapd.conf
+  else
+    sudo sed -i "/ssid\=/c\ssid\=ArkOS_AP" /etc/hostapd/hostapd.conf
+  fi
   if [ ! -z ${host_ap_masked} ]; then
     sudo systemctl unmask hostapd.service
     sudo systemctl disable hostapd.service
@@ -70,6 +52,7 @@ if [[ -z ${cmd} ]]; then
   printf "\nNo valid argument provided such as Enable or Disable\n\n"
   exit 1
 fi
-$cmd
+$cmd $1
 
 exit 0
+
