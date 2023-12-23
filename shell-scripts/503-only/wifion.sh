97d6cd49ae824798752a6d59e02f6d0a84e9fcbd
christianhaitian <mckman@comcast.net>
2023-12-23 01:30:15 -0500

    Update wifion.sh to no longer remove the wifi module.  Use rfkill instead.

--- shell-scripts/503-only/wifion.sh
+++ shell-scripts/503-only/wifion.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 #c_brightness="$(cat /sys/devices/platform/backlight/backlight/backlight/brightness)"
 printf "\n\n\n\e[32mEnabling wifi and related services...\n"
-sudo modprobe -r 8821cs
-sudo modprobe -i 8821cs
+#sudo modprobe -r 8821cs
+#sudo modprobe -i 8821cs
 #sudo systemctl start NetworkManager
 #sudo nmcli n on
-sudo sed -i '/blacklist 8821cs/d' /etc/modprobe.d/blacklist.conf
+#sudo sed -i '/blacklist 8821cs/d' /etc/modprobe.d/blacklist.conf
+sudo rfkill unblock wlan
+#sudo systemctl disable networkwatchdaemon
+#sudo systemctl stop networkwatchdaemon
 sudo systemctl enable NetworkManager
-sudo systemctl enable networkwatchdaemon
-printf "\n\n\n\e[32mWifi has been enabled.  Now rebooting.\n"
+sudo systemctl start NetworkManager
+#sudo systemctl enable networkwatchdaemon
+printf "\n\n\n\e[32mWifi has been enabled.\n"
+#printf "\n\n\n\e[32mWifi has been enabled.  Now rebooting.\n"
 sleep 2
-sudo reboot
+#sudo reboot
 #echo 3 > /sys/class/backlight/backlight/brightness
 #sleep 1
 #echo $c_brightness > /sys/class/backlight/backlight/brightness
-
