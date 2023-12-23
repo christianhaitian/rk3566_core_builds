62714b918f65e9f0f911041c6de1b5def6c6ec29
christianhaitian <mckman@comcast.net>
2023-12-22 19:42:44 -0500

    Update volume.sh to include support for swap volume buttons

--- shell-scripts/503-only/volume.sh
+++ shell-scripts/503-only/volume.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
+if [ -e "/home/ark/.config/.SWAPVOLUMEBUTTONS" ] && [[ "${1}" != *"%"* ]]; then
+  if [[ "${1}" == *"+"* ]]; then
+    value="$(sed '/+/s//-/' <<< $1)"
+  else
+    value="$(sed '/-/s//+/' <<< $1)"
+  fi
+else
+  value="${1}"
+fi
+
 if test ! -z "$(cat /home/ark/.asoundrc | grep softvol)"
 then
-  amixer -q sset Master ${1}
+  amixer -q sset Master ${value}
 else
   pcms=$(bluealsa-cli list-pcms)
 
@@ -11,14 +21,14 @@ else
   done
 
   for pcm in $pcms; do
-    if [[ "${1}" == *"-"* ]]; then
+    if [[ "${value}" == *"-"* ]]; then
       newVol=$(( $curVol - 1 ))
       if (( $newVol >= 0 )); then
         bluealsa-cli volume ${pcm} ${newVol}
       else
         echo "Minimum volume reached"
       fi
-    elif [[ "${1}" == *"+"* ]]; then
+    elif [[ "${value}" == *"+"* ]]; then
       newVol=$(( $curVol + 1 ))
       if (( $newVol <= 127 )); then
         bluealsa-cli volume ${pcm} ${newVol}
