efdf361d20420395e46c83f41f51415fa91aa0ad
christianhaitian <mckman@comcast.net>
2024-09-03 16:44:44 -0400

    Update Restore ArkOS Settings.sh to better handle restore of settings
    Fixes restoring the following:
    
    Game Loading image
    Switch button tap to off
    Timezone
    Verbal Battery Warning

--- shell-scripts/Restore ArkOS Settings.sh
+++ shell-scripts/Restore ArkOS Settings.sh
@@ -28,6 +28,48 @@ do
 
 		sudo tar --same-owner -zxhvf /roms/backup/arkosbackup.tar.gz -C / | tee -a "$LOG_FILE"
 		if [ $? -eq 0 ]; then
+			# Properly restore previously set timezone
+			[ -f /dev/shm/TZ ] && ln -sf /usr/share/zoneinfo/$(cat /dev/shm/TZ) /etc/localtime && sudo rm /dev/shm/TZ
+			# Properly restore preferred Switch button tap to off state
+			if [ ! -z $(grep '<bool name="InvertPwrBtn" value="true" />' /home/ark/.emulationstation/es_settings.cfg | tr -d '\0') ]; then
+			  [ -z $(find /home/ark/.config/.SWAPPOWERANDSUSPEND 2>/dev/null) ] && touch /home/ark/.config/.SWAPPOWERANDSUSPEND
+			fi
+			# Properly restore preferred Verbal Battery Warning state
+			if [ ! -z $(grep '<string name="VerbalBatteryWarning" value="on" />' /home/ark/.emulationstation/es_settings.cfg | tr -d '\0') ]; then
+			  [ ! -z $(find /home/ark/.config/.NOVERBALBATTERYWARNING 2>/dev/null) ] && rm /home/ark/.config/.NOVERBALBATTERYWARNING 2>/dev/null
+			  if [ ! -z $(grep '<string name="VerbalBatteryVoice" value="male1" />' /home/ark/.emulationstation/es_settings.cfg | tr -d '\0') ]; then
+			    [ ! -z $(find /home/ark/.config/.MBROLA_VOICE* 2>/dev/null) ] && rm /home/ark/.config/.MBROLA_VOICE* 2>/dev/null
+			  elif [ ! -z $(grep '<string name="VerbalBatteryVoice" value="male2" />' /home/ark/.emulationstation/es_settings.cfg | tr -d '\0') ]; then
+			    [ ! -z $(find /home/ark/.config/.MBROLA_VOICE* 2>/dev/null) ] && rm /home/ark/.config/.MBROLA_VOICE* 2>/dev/null
+			    touch /home/ark/.config/.MBROLA_VOICE_MALE3
+			  else
+			    [ ! -z $(find /home/ark/.config/.MBROLA_VOICE* 2>/dev/null) ] && rm /home/ark/.config/.MBROLA_VOICE* 2>/dev/null
+			    touch /home/ark/.config/.MBROLA_VOICE_FEMALE
+			  fi
+			fi
+			# Properly restore preferred Game Loading image state
+			if [ ! -z $(grep '<string name="GameLoadingImageMode" value="ascii" />' /home/ark/.emulationstation/es_settings.cfg | tr -d '\0') ]; then
+			  [ ! -z $(find /home/ark/.config/.GameLoadingIMode* 2>/dev/null) ] && rm /home/ark/.config/.GameLoadingIMode* 2>/dev/null
+			  touch /home/ark/.config/.GameLoadingIModeASCII
+			elif [ ! -z $(grep '<string name="GameLoadingImageMode" value="pic" />' /home/ark/.emulationstation/es_settings.cfg | tr -d '\0') ]; then
+			  [ ! -z $(find /home/ark/.config/.GameLoadingIMode* 2>/dev/null) ] && rm /home/ark/.config/.GameLoadingIMode* 2>/dev/null
+			  touch /home/ark/.config/.GameLoadingIModePIC
+			  if [ ! -z $(grep '<string name="GameLoadingImage" value="default" />' /home/ark/.emulationstation/es_settings.cfg | tr -d '\0') ]; then
+			    [ ! -z $(find /home/ark/.config/.LOADING_IMAGE* 2>/dev/null) ] && rm /home/ark/.config/.LOADING_IMAGE* 2>/dev/null
+			  elif [ ! -z $(grep '<string name="GameLoadingImage" value="marquee" />' /home/ark/.emulationstation/es_settings.cfg | tr -d '\0') ]; then
+			    [ ! -z $(find /home/ark/.config/.LOADING_IMAGE* 2>/dev/null) ] && rm /home/ark/.config/.LOADING_IMAGE* 2>/dev/null
+				touch /home/ark/.config/.LOADING_IMAGE_MARQUEE
+			  elif [ ! -z $(grep '<string name="GameLoadingImage" value="image" />' /home/ark/.emulationstation/es_settings.cfg | tr -d '\0') ]; then
+			    [ ! -z $(find /home/ark/.config/.LOADING_IMAGE* 2>/dev/null) ] && rm /home/ark/.config/.LOADING_IMAGE* 2>/dev/null
+				touch /home/ark/.config/.LOADING_IMAGE_IMAGE
+			  else
+			    [ ! -z $(find /home/ark/.config/.LOADING_IMAGE* 2>/dev/null) ] && rm /home/ark/.config/.LOADING_IMAGE* 2>/dev/null
+				touch /home/ark/.config/.LOADING_IMAGE_THUMB
+			  fi
+			else
+			  [ ! -z $(find /home/ark/.config/.GameLoadingIMode* 2>/dev/null) ] && rm /home/ark/.config/.GameLoadingIMode* 2>/dev/null
+			  touch /home/ark/.config/.GameLoadingIModeNO
+			fi
 			printf "\n\n\e[32mThe restore completed successfuly. \nYou will need to reboot your system in order for your restored settings to take effect! \n" | tee -a "$LOG_FILE"
 			printf "\033[0m" | tee -a "$LOG_FILE"
 			sleep 1
