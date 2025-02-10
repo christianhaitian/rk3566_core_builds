b60562f7fee685402666e16e0de59e69a337664a
christianhaitian <mckman@comcast.net>
2025-02-09 21:37:28 -0500

    Update Kodi.sh to add python3.7 support to urllib3 script module for importlib_metadata

--- shell-scripts/503-only/Kodi.sh
+++ shell-scripts/503-only/Kodi.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if [ -z "$(grep importlib_metadata /home/ark/.kodi/addons/script.module.urllib3/lib/urllib3/http2/__init__.py | tr -d '\0')" ]; then
+  sed -i '/from importlib.metadata import version/c\try:\n  from importlib.metadata import version\nexcept:\n  from importlib_metadata import version' /home/ark/.kodi/addons/script.module.urllib3/lib/urllib3/http2/__init__.py
+fi
+
 export KODI_HOME=/opt/kodi/share/kodi
 #export PYTHONHOME=/opt/kodi/lib/kodi/kodi-gbm
 export PYTHONPATH=/lib/python3.7/
@@ -11,8 +15,16 @@ sudo perfmax kodi
 export MALLOC_MMAP_THRESHOLD_=131072
 
 # Change owner of volume keys or Kodi will grab them
-sudo chown root:root /dev/input/event2
-sudo chown root:root /dev/input/event0
+#if [ "$(cat ~/.config/.DEVICE)" != "RG503" ]; then
+  if [[ -e "/dev/input/by-path/platform-fe5b0000.i2c-event" ]]; then
+    sudo chown root:root /dev/input/event3
+  else
+    sudo chown root:root /dev/input/event2
+  fi
+  sudo chown root:root /dev/input/event0
+#else
+ #sudo chown root:root /dev/input/event1
+#fi
 
 printf "\n\n\e[32mStarting Kodi.  Please wait..."
 export LD_LIBRARY_PATH=/opt/kodi/libs/:/opt/kodi/libs/samba
