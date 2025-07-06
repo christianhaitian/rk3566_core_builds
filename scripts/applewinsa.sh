7a269627f30e4833294e2f054fd95f5d9aca60b4
christianhaitian <mckman@comcast.net>
2025-07-06 18:35:34 -0400

    Update applewinsa.sh to accommodate debian script building

--- scripts/applewinsa.sh
+++ scripts/applewinsa.sh
@@ -70,8 +70,10 @@ bitness="$(getconf LONG_BIT)"
 	  done
 	  fi
 
-           update-alternatives --set gcc "/usr/local/bin/gcc"
-           update-alternatives --set g++ "/usr/local/bin/g++"
+           if [[ "$0" == *"builds-alt"* ]]; then
+             update-alternatives --set gcc "/usr/local/bin/gcc"
+             update-alternatives --set g++ "/usr/local/bin/g++"
+	   fi
            if [ ! -d "build" ]; then
              mkdir build
              cd build
@@ -86,14 +88,18 @@ bitness="$(getconf LONG_BIT)"
            make -j$(nproc)
 
            if [[ $? != "0" ]]; then
-		     echo " "
-             update-alternatives --set gcc "/usr/bin/gcc-8"
-             update-alternatives --set g++ "/usr/bin/g++-8"
-		     echo "There was an error that occured while making the applewin standalone.  Stopping here."
+	     echo " "
+             if [[ "$0" == *"builds-alt"* ]]; then
+               update-alternatives --set gcc "/usr/bin/gcc-8"
+               update-alternatives --set g++ "/usr/bin/g++-8"
+	     fi
+	     echo "There was an error that occured while making the applewin standalone.  Stopping here."
              exit 1
            fi
-           update-alternatives --set gcc "/usr/bin/gcc-8"
-           update-alternatives --set g++ "/usr/bin/g++-8"
+	   if [[ "$0" == *"builds-alt"* ]]; then
+             update-alternatives --set gcc "/usr/bin/gcc-8"
+             update-alternatives --set g++ "/usr/bin/g++-8"
+	   fi
            strip sa2
 
            if [ ! -d "../../applewinsa-$bitness/" ]; then
